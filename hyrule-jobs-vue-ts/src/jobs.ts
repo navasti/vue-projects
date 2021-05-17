4dc12d097d936149fa9f32682bd79e8f8151ec2e
Paweł Szostak <szostak_pawel@outlook.com>
2021-05-17 22:17:57 +0200

    Added types and data resource

---
+++ hyrule-jobs-vue-ts/src/jobs.ts
@@ -0,0 +1,29 @@
+const jobs = [
+  {
+    title: "farm worker",
+    location: "lon lon ranch",
+    salary: 30000,
+    id: "1",
+  },
+  {
+    title: "quarryman",
+    location: "death mountain",
+    salary: 40000,
+    id: "2",
+  },
+  {
+    title: "flute player",
+    location: "the lost woods",
+    salary: 35000,
+    id: "3",
+  },
+  { title: "fisherman", location: "lake hylia", salary: 21000, id: "4" },
+  {
+    title: "prison guard",
+    location: "gerudo valley",
+    salary: 32000,
+    id: "5",
+  },
+];
+
+export default jobs;
