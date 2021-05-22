cdb9693003bd3f390464f4009fb31a1dcb651903
Paweł Szostak <szostak_pawel@outlook.com>
2021-05-22 08:54:58 +0200

    Initial chrip front part project

---
+++ chirp/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style lang="scss"></style>
---
+++ chirp/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <p>home</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {},
+});
+</script>
